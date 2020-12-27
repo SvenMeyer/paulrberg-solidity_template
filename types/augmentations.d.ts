dcd8a70327a44e627c5eab053b6a3a95ac58ffd0
Sven Meyer <git_dev@svenmeyer.com>
2020-12-27 20:43:28 +1100

    fix: merge pull request zemse-pr

--- types/augmentations.d.ts
+++ types/augmentations.d.ts
@@ -1,5 +1,6 @@
 import { Accounts, Signers } from "./";
-import { Greeter } from "../typechain/Greeter";
+
+import { Greeter } from "../typechain";
 
 declare module "mocha" {
   export interface Context {
