7dd147c76bd66571dfe451c00d9b247f276f095d
Sven Meyer <git_dev@svenmeyer.com>
2020-12-29 12:40:08 +1100

    test: simplify

--- test/Greeter.behavior.ts
+++ test/Greeter.behavior.ts
@@ -2,9 +2,8 @@ import { expect } from "chai";
 
 export function shouldBehaveLikeGreeter(): void {
   it("should return the new greeting once it's changed", async function () {
-    expect(await this.greeter.connect(this.signers.admin).greet()).to.equal("Hello, world!");
+    expect(await this.greeter.greet()).to.equal("Hello, world!");
 
-    await this.greeter.setGreeting("Hola, mundo!");
-    expect(await this.greeter.connect(this.signers.admin).greet()).to.equal("Hola, mundo!");
+    expect(await this.greeter.greet("New Message")).to.equal("New Message");
   });
 }
